56d399f37a03aeec9ee032fba8365ee68bc7c3fb
Md Asif Khan <57280266+Asif520@users.noreply.github.com>
2026-02-05 12:58:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const micIdleIcon = '<i class="fas fa-microphone"></i>';
 const micRecordingIcon = '<i class="fas fa-circle" style="color:red;"></i>';
 
 const sessionId = crypto.randomUUID();
-const ws = new WebSocket(`wss://asif988-sts-stt-backend.hf.space//ws/sts/${sessionId}`);
+const ws = new WebSocket(`wss://asif988-sts-stt-backend.hf.space/ws/sts/${sessionId}`);
 ws.binaryType = "arraybuffer";
 
 const activeVisualizers = new Map(); // audio element → { analyser, animationId }
@@ -441,4 +441,5 @@ clearBtn.onclick = () => {
     chat.innerHTML = "";
   }
 };
+
 
